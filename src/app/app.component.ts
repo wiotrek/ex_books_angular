251b87ffb71d6bed2b8cf15a100a235124befc5d
wiotrek <piotrwaszak97@gmail.com>
2020-09-26 18:34:01 +0200

    new branch depoy, and edit detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { BooksService } from './books.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    private bs: BooksService
+  ){}
   title = 'books';
+  isLogin: boolean = this.bs.isLogIn;
+
+  logout = () => {
+    localStorage.removeItem('userToken');
+    localStorage.clear();
+    window.location.href = this.bs.localhost;
+  }
+
 }
