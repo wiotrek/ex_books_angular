d8c318412513b79f0636adfc0c99efeeac052e2c
wiotrek <piotrwaszak97@gmail.com>
2020-10-01 16:27:41 +0200

    connect select/option in create post where options are from rest api

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -18,13 +18,29 @@ export class ListComponent implements OnInit {
   public pageNext: string;
   public p = 1;
   public isLogIn = this.bs.isLogIn;
+  // genreBooks: Map <number, string>;
+  genreBooks;
 
   ngOnInit(): void {
     this.getBooks();
+    console.log(this.genreBooks);
     this.postBook = new FormGroup({
       title: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(32)]),
-      description: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(256)])
+      description: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]),
+      genre: new FormControl('', [Validators.required])
     });
+  }
+
+  getBooks = () => {
+    this.bs.allBooks().subscribe(
+      response => {
+        this.showBooks = response;
+
+        this.genreBooks = new Map(response[0].GENRE);
+        // this.genreBooks = response[0].GENRE;
+        console.log(this.genreBooks);
+      }
+    );
   }
 
   createPost = () => {
@@ -39,12 +55,5 @@ export class ListComponent implements OnInit {
     );
   }
 
-  getBooks = () => {
-    this.bs.allBooks().subscribe(
-      response => {
-        this.showBooks = response;
-      }
-    );
-  }
 }
 
